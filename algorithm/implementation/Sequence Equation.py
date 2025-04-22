70b797c240c7f135027f7b5c45553ab0724caad9
Mingming Li <ml@mingml.com>
2025-04-22 08:42:58 -0400

    Algorithm: implementation;

---
+++ algorithm/implementation/Sequence Equation.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'permutationEquation' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY p as parameter.
+#
+
+def permutationEquation(p):
+    # Write your code here
+    d = {val: idx+1 for idx, val in enumerate(p)}
+
+    result = []
+    for i in range(1, len(p)+1):
+        px = d[i]
+        y = d[px]
+        result.append(y)
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = list(map(int, input().rstrip().split()))
+
+    result = permutationEquation(p)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
